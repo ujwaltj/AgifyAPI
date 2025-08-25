45bfd36e066920628059f375a2865891a5b02615
Ujwal B <ujwal.tj7@gmail.com>
2025-08-25 18:11:31 +1000

    Updated the README.md

--- src/test/steps/agePredict.ts
+++ src/test/steps/agePredict.ts
@@ -4,48 +4,50 @@ import { request as pwRequest, APIResponse } from "@playwright/test";
 
 let apiResponse: APIResponse;
 let nameParam: string | undefined;
-let response : any
+let response: any
 Given('I have a name {string}', async function (name: string) {
-           // Write code here that turns the phrase above into concrete actions
-           nameParam = name;
-         });
+    // Write code here that turns the phrase above into concrete actions
+    console.log("Scenario 1");
+    nameParam = name;
+});
 
 When('I request the age prediction from the API', async function () {
-        const apiContext = await pwRequest.newContext();
-           const url = nameParam
-            ? `https://api.agify.io/?name=${nameParam}`
-            : `https://api.agify.io/`; // no parameter case
-         apiResponse = await apiContext.get(url);
-         });        
-         
- Then('the response status should be {int}', async function (statusCode: number) {
-        console.log("The status code is", statusCode);
-        expect(apiResponse.status()).toBe(statusCode);
-         });         
+    const apiContext = await pwRequest.newContext();
+    const url = nameParam
+        ? `https://api.agify.io/?name=${nameParam}`
+        : `https://api.agify.io/`; // no parameter case
+    apiResponse = await apiContext.get(url);
+});
 
- Then("the response should contain an {string}", async function (field: string) {
-        const body = await apiResponse.json();
-        console.log(body[field]);
-        expect(body).toHaveProperty(field);
-});         
+Then('the response status should be {int}', async function (statusCode: number) {
+    console.log("The status code is", statusCode);
+    expect(apiResponse.status()).toBe(statusCode);
+});
+
+Then("the response should contain an {string}", async function (field: string) {
+    const body = await apiResponse.json();
+    console.log(`The name ${body.name} has the age ${body[field]}`);
+    expect(body).toHaveProperty(field);
+
+});
 
 Given('I have names {string}', async function (inputName: string) {
-          nameParam = inputName;
-         });
+    console.log("Scenario 2");
+    nameParam = inputName;
+});
 
 When('I request age from the Agify API', async function () {
-           const apiContext = await pwRequest.newContext();
-           response = await apiContext.get(`https://api.agify.io?name=${nameParam}`)
-         });
-         
- Then('I should get an age prediction for name', async function () {
-           const body = await response.json();
-           console.log(body);
-           
-           expect(body).toHaveProperty("name");
-            expect(body).toHaveProperty("age");
-            expect(body).toHaveProperty("count");
-           //console.log(`The name ${body.name} has the age ${body.age}`);
-        });
+    const apiContext = await pwRequest.newContext();
+    response = await apiContext.get(`https://api.agify.io?name=${nameParam}`)
+});
 
-         
+Then('I should get an age prediction for name', async function () {
+    const body = await response.json();
+    console.log(body);
+
+    expect(body).toHaveProperty("name");
+    expect(body).toHaveProperty("age");
+    expect(body).toHaveProperty("count");
+    //console.log(`The name ${body.name} has the age ${body.age}`);
+});
+
